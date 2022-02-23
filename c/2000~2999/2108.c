ecf9631ca2adaaca3cf29d83f16b15a9c7312bff
iijung <987kmj4@naver.com>
2022-02-23 23:26:43 +0900

    2108 (재채점)

--- c/2000~2999/2108.c
+++ c/2000~2999/2108.c
@@ -14,15 +14,15 @@ int main()
 
 	double sum = 0;
 	int count[8001] = {0};
-	int max = 0, min = 0;
+	int max = 0x80000000, min = 0x7FFFFFFF;
 	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", &Arr[i]);
 
 		sum += Arr[i];
 		count[Arr[i]+4000]++;
-		if (!max || max < Arr[i]) max = Arr[i];
-		if (!min || min > Arr[i]) min = Arr[i];
+		if (max < Arr[i]) max = Arr[i];
+		if (min > Arr[i]) min = Arr[i];
 	}
 
 	qsort(Arr, n, sizeof(int), compare);
@@ -42,7 +42,11 @@ int main()
 		}
 	}
 
-	printf("%.0f\n", sum /n);
+	double avg = sum / (double)n;
+	if (avg < 0) avg -= 0.5;
+	else if (avg > 0) avg += 0.5;
+
+	printf("%d\n", (int)avg);
 	printf("%d\n", Arr[n/2]);
 	printf("%d\n", fre[1] - 4000);
 	printf("%d\n", max - min);
