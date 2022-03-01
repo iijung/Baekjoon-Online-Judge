6c6b4f48a5f2daf7f182ef20cab00752e091a9d8
iijung <987kmj4@naver.com>
2022-03-01 20:20:11 +0900

    18258

---
+++ c/18000~18999/18258.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 2000000
+#define NEXT(x) { x = (x + 1) % MAX; }
+
+int Front = -1;
+int Back = -1;
+int Queue[MAX];
+
+int empty() { return Front == Back; }
+int size() { return (MAX + Back - Front) % MAX; }
+int front() { if (empty()) return -1; return Queue[Front + 1]; }
+int back() { if (empty()) return -1; return Queue[Back]; }
+int pop() { if (empty()) return -1; NEXT(Front); return Queue[Front]; }
+void push(int x) { NEXT(Back); Queue[Back] = x; }
+
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	int x = 0;
+	char cmd[6] = {0};
+	while (n--)
+	{
+		scanf("%s", cmd);
+		getchar();
+
+		if (!strcmp(cmd,"push")) {
+			scanf("%d", &x);
+			push(x);
+		} else if (!strcmp(cmd, "pop")) {
+			printf("%d\n", pop());
+		} else if (!strcmp(cmd, "size")) {
+			printf("%d\n", size());
+		} else if (!strcmp(cmd, "empty")) {
+			printf("%d\n", empty());
+		} else if (!strcmp(cmd, "front")) {
+			printf("%d\n", front());
+		} else if (!strcmp(cmd, "back")) {
+			printf("%d\n", back());
+		}
+	}
+	return 0;
+}
