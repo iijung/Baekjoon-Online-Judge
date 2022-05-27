96e9ef57cb0a80cae3e20119f1ed18d458db15a7
iijung <987kmj4@naver.com>
2022-05-28 00:16:21 +0900

    숏코드 추가

--- c/2000~2999/2447.c
+++ c/2000~2999/2447.c
@@ -22,3 +22,4 @@ int main()
 	}
 	return 0;
 }
+//x,y;void f(int n){if((x/n)%3==1&&(y/n)%3==1)putchar(32);else if(n/3==0)putchar(42);else f(n/3);}i;main(n){scanf("%d",&n);while(i<n*n){x=i/n;y=i%n;f(n);if(++i%n==0)puts("");}}
