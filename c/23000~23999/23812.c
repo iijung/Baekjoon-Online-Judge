921fa5dccd49a7da062da00aef536d43083c2c04
iijung <987kmj4@naver.com>
2022-06-02 22:26:16 +0900

    23812

---
+++ c/23000~23999/23812.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+char Arr[2][501];
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	memset(Arr[0], '@', n * 5);
+	memset(Arr[1], '@', n * 5);
+	memset(Arr[1] + n, ' ', n * 3);
+	for (int i = 0; i < n; i++) puts(Arr[1]);
+	for (int i = 0; i < n; i++) puts(Arr[0]);
+	for (int i = 0; i < n; i++) puts(Arr[1]);
+	for (int i = 0; i < n; i++) puts(Arr[0]);
+	for (int i = 0; i < n; i++) puts(Arr[1]);
+	return (0);
+}
