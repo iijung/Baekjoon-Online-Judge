6c0de10ad2c43987b4e65d2eeab6f1f9488e4ad6
iijung <987kmj4@naver.com>
2022-06-05 02:12:02 +0900

    24369

---
+++ c/24000~24999/24369.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+int o(int a, int b, int c, int n0)
+{
+	double d = b * b - 4 * a * c;
+	if (a > 0)
+		return (0);
+	if (a == 0)
+	{
+		if (b > 0)
+			return (0);
+		if (b == 0)
+			return (c <= 0? 1 : 0);
+		if (b < 0)
+			return (b * n0 + c <= 0? 1 : 0);
+	}
+	if (d <= 0)
+		return (1);
+	double t1 = (-b - sqrt(d)) / 2 / a;
+	double t2 = (-b + sqrt(d)) / 2 / a;
+	double max = t1 > t2? t1 : t2;
+	return (max <= n0? 1 : 0);
+}
+
+int main()
+{
+	int a2 = 0, a1 = 0, a0 = 0;
+	scanf("%d %d %d", &a2, &a1, &a0);
+
+	int c = 0;
+	scanf("%d", &c);
+
+	int n0 = 0;
+	scanf("%d", &n0);
+
+	printf("%d", o(c - a2, -a1, -a0, n0));
+	return (0);
+}
